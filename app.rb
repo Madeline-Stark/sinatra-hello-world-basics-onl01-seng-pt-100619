f15ed1775a691634339385cf3256f3d30fd6df5b
Madeline Stark <madeline.stark@flatironschool.com>
2020-01-28 20:02:52 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,16 @@
 class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
-
+  get '/' do 
+    'hello world'
+  end
+  
+  get '/wendy' do 
+    erb :wendy
+  end
+  
+  get '/list' do
+     erb :'list'
+  end
 
 end
